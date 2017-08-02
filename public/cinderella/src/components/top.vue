ea3097fe73c8f59e2353034dc398dd4f07cb2982
matzz <matsubaray@eac.ne.jp>
2017-08-02 19:04:17 +0900

    wip

---
+++ public/cinderella/src/components/top.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    hogehogeodasa1243
+  </div>
+</template>
+<script>
+//  import firebase from 'firebase';
+
+  export default {
+    data() {
+      return {};
+    },
+
+    methods: {
+
+    },
+
+    mounted() {
+//      const db = firebase.database();
+    },
+  };
+</script>
