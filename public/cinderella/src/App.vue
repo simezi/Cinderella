2e5766a0b64e1d49ddadb66371ea4a8361d7e1f4
matzz <matsubaray@eac.ne.jp>
2017-08-26 12:41:21 +0900

    vuetify

--- public/cinderella/src/App.vue
+++ public/cinderella/src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-navigation-drawer></v-navigation-drawer>
+    <v-toolbar></v-toolbar>
+    <main>
+      <v-container fluid>
+        <div id="app">
+          <router-view></router-view>
+        </div>
+      </v-container>
+    </main>
+    <v-footer></v-footer>
+  </v-app>
 </template>
 
 <script>
+
 export default {
   name: 'app',
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style module lang="postcss">
 </style>
