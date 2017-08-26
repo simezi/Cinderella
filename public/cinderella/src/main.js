e4a14cd31aa20c1c2a60c097b40c2a7505303245
matzz <matsubaray@eac.ne.jp>
2017-08-26 19:11:43 +0900

    integrate with firebase

--- public/cinderella/src/main.js
+++ public/cinderella/src/main.js
@@ -3,12 +3,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import Vuex from 'vuex';
+import firebase from 'firebase';
 import App from './App';
 import router from './router';
 
 import('vuetify/dist/vuetify.min.css');
 
 
+const config = {
+  // apiKey: process.env.firebaseApiKey,
+  apiKey: 'AIzaSyByLHuxZDx-S2XRSMNLBki_IlhiAWdzetw',
+  authDomain: 'cinderella-c117d.firebaseapp.com',
+  databaseURL: 'https://cinderella-c117d.firebaseio.com',
+  storageBucket: 'cinderella-c117d.appspot.com',
+};
+firebase.initializeApp(config);
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify);
