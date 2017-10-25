f691e3c8d52e5e703aeac6df626905faffbf80c0
matzz <matsubaray@eac.ne.jp>
2017-10-25 07:53:23 -0700

    adjust

--- public/cinderella/src/sw.js
+++ public/cinderella/src/sw.js
@@ -3,5 +3,4 @@ importScripts('workbox-sw.prod.v2.1.0.js');
 const workboxSW = new self.WorkboxSW();
 workboxSW.precache([]);
 
-workboxSW.router.registerRoute(
-  '/js/:filename', workboxSW.strategies.cacheFirst());
+workboxSW.router.registerRoute('/js/:filename', workboxSW.strategies.cacheFirst());
